e8d9176aa47f69def0cbd8ac1bc811e435a304e9
trevbest14 <150987625+trevbest14@users.noreply.github.com>
2024-04-02 19:19:40 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import {useState} from 'react'
+import {puppyList} from './data.js'
+import './path-to-css.css'
+import './App.css'
+
+function App() {
+
+const [puppies, setPuppies] = useState(puppyList)
+const [featPupId, setFeatPupId] = useState(null)
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+const [puppy] = useState(puppyList)
+console.log(featuredPup)
+console.log(setPuppies)
+
+console.log("puppyList: ", puppyList);
+console.log("puppy.id:" , puppy.id)
+
+return (
+  <div className="App">
+      {
+        puppies.map((puppy) => {
+             return (
+             <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+             );
+           })}
+           { featPupId && <p>{featPupId}</p>}
+           </div>
+)(
+         <featPupId> 
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+          </featPupId>
+     
+        )}
+
+export default App
